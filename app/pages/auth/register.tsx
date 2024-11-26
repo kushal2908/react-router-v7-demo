f21a28facd31156dce44f9a7b1d5c46a018d2506
Safa <sardersafabinsalam@arogga.net>
2024-11-26 18:05:13 +0600

    UI updated

--- app/pages/auth/register.tsx
+++ app/pages/auth/register.tsx
@@ -8,11 +8,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Register({}: Props) {
+  const submit = async () => {
+    const res = await fetch(`https://reqres.in/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="h-[calc(100vh-64px)] flex items-center justify-center">
       <div className="w-[320px] min-h-200px] bg-white p-4 border border-gray-300 rounded-md shadow-lg">
         <h2 className="text-xl font-semibold mb-4 text-gray-900">Register</h2>
-        <Form action="/auth/register" method="post" className="mb-4">
+        <Form className="mb-4">
           <input
             type="text"
             className="w-full p-2 mb-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-xs"
@@ -25,7 +39,11 @@ export default function Register({}: Props) {
             placeholder="Password"
             name="password"
           />
-          <button type="submit" className="w-full bg-teal-500 p-2 rounded-md text-white hover:bg-teal-600 text-sm">
+          <button
+            type="submit"
+            className="w-full bg-teal-500 p-2 rounded-md text-white hover:bg-teal-600 text-sm"
+            onClick={submit}
+          >
             Register
           </button>
         </Form>
--- app/pages/blogs/index.tsx
+++ app/pages/blogs/index.tsx
@@ -18,7 +18,7 @@ export default function index({ loaderData }: Route.ComponentProps) {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {loaderData?.map((blog: any) => (
           <Link key={blog.id} to={`/blogs/${blog.id}`}>
-            <div className="bg-white min-h-[250px] p-4 border border-gray-300 rounded-md hover:shadow-lg hover:border-teal-200 hover:ring-4 hover:ring-teal-50 transition-all">
+            <div className="bg-white min-h-[250px] p-4 border border-gray-300 rounded-md hover:shadow-lg hover:border-gray-400 hover:scale-[1.02]  transition-all">
               <h2 className="text-lg font-semibold mb-2 text-gray-800">
                 {blog.id}
                 {". "}
